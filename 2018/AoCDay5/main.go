08d54d8e872fee29094e0624860dd1a53cf47ed0
Aaron Clawson <clawsona@amazon.com>
2018-12-05 09:22:30 -0600

    Day5 iterativly

---
+++ 2018/AoCDay5/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/MadVikingGod/AdventOfCode/2018/helpers"
+)
+
+func main() {
+	inputs, err := helpers.GetInput(5)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	input := []byte(inputs[0])
+	fmt.Println(input[:5])
+
+	out, cont := react(input)
+	for cont {
+		out, cont = react(out)
+	}
+	//fmt.Println(string(out))
+	fmt.Println(len(out))
+
+	shortest := len(out)
+	for _, char := range []byte("abcdefghijklmnopqrstuvwxyz") {
+		out, cont = react(remove(input, char))
+		for cont {
+			out, cont = react(out)
+		}
+		if len(out) < shortest {
+			shortest = len(out)
+		}
+
+		fmt.Println(string(char), len(out))
+	}
+	fmt.Println(shortest)
+}
+
+func react(input []byte) ([]byte, bool) {
+	for i := 1; i < len(input); i++ {
+		if input[i-1]^byte(' ') == input[i] {
+			return append(input[:i-1], input[i+1:]...), true
+		}
+	}
+	return input, false
+}
+
+func remove(input []byte, char byte) []byte {
+	i := string(input)
+	return []byte(strings.Replace(strings.Replace(i, string(char), "", -1), string(char^byte(' ')), "", -1))
+}
