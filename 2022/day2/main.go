2580c2bb8dda3ca61a2a5380c013fec0e4bd423f
Aaron Clawson <3766680+MadVikingGod@users.noreply.github.com>
2022-12-02 21:00:06 -0600

    refactored day 2
    Signed-off-by: Aaron Clawson <3766680+MadVikingGod@users.noreply.github.com>

--- 2022/day2/main.go
+++ 2022/day2/main.go
@@ -9,120 +9,52 @@ import (
 var input string
 
 func main() {
-	maxCount := 0
-	for _, selfMap := range selfMaps {
-		count := match(input, selfMap)
-		if count > maxCount {
-			maxCount = count
-		}
-	}
-	println(maxCount)
+	println(match(input))
 
 	println(match2(input))
 }
 
-func match(input string, selfMap map[string]string) int {
+func match(input string) int {
 
+	transform := map[string]int{
+		"A X": 1 + 3,
+		"A Y": 2 + 6,
+		"A Z": 3 + 0,
+		"B X": 1 + 0,
+		"B Y": 2 + 3,
+		"B Z": 3 + 6,
+		"C X": 1 + 6,
+		"C Y": 2 + 0,
+		"C Z": 3 + 3,
+	}
 	count := 0
 	for _, line := range strings.Split(input, "\n") {
 		if line == "" {
 			continue
 		}
-		parts := strings.Split(line, " ")
-		opp := opponentMap[parts[0]]
-		self := selfMap[parts[1]]
-		count += round(opp, self)
+		count += transform[line]
 	}
 	return count
 }
 
 func match2(input string) int {
-	transform := map[string]map[string]int{
-		"X": { //Lose
-			"A": 3,
-			"B": 1,
-			"C": 2,
-		},
-		"Y": { //Draw
-			"A": 1 + 3,
-			"B": 2 + 3,
-			"C": 3 + 3,
-		},
-		"Z": { //Win
-			"A": 2 + 6,
-			"B": 3 + 6,
-			"C": 1 + 6,
-		},
+	transform := map[string]int{
+		"A X": 3 + 0,
+		"A Y": 1 + 3,
+		"A Z": 2 + 6,
+		"B X": 1 + 0,
+		"B Y": 2 + 3,
+		"B Z": 3 + 6,
+		"C X": 2 + 0,
+		"C Y": 3 + 3,
+		"C Z": 1 + 6,
 	}
 	count := 0
 	for _, line := range strings.Split(input, "\n") {
 		if line == "" {
 			continue
 		}
-		parts := strings.Split(line, " ")
-		count += transform[parts[1]][parts[0]]
+		count += transform[line]
 	}
 	return count
-}
-
-func round(opp, self string) int {
-	// Win is worth 6 points
-	// Draw is worth 3 points
-	// Loss is worth 0 points
-	// R is worth 1 point
-	// P is worth 2 points
-	// S is worth 3 points
-	throw := map[string]int{
-		"R": 1,
-		"P": 2,
-		"S": 3,
-	}
-	if opp == self {
-		return 3 + throw[self]
-	}
-	if (opp == "R" && self == "P") || (opp == "P" && self == "S") || (opp == "S" && self == "R") {
-		return 6 + throw[self]
-	}
-	return 0 + throw[self]
-
-}
-
-// tables
-var opponentMap = map[string]string{
-	"A": "R",
-	"B": "P",
-	"C": "S",
-}
-
-var selfMaps = []map[string]string{
-	{
-		"X": "R",
-		"Y": "P",
-		"Z": "S",
-	},
-	{
-		"X": "R",
-		"Y": "S",
-		"Z": "P",
-	},
-	{
-		"X": "P",
-		"Y": "R",
-		"Z": "S",
-	},
-	{
-		"X": "P",
-		"Y": "S",
-		"Z": "R",
-	},
-	{
-		"X": "S",
-		"Y": "R",
-		"Z": "P",
-	},
-	{
-		"X": "S",
-		"Y": "P",
-		"Z": "R",
-	},
 }
