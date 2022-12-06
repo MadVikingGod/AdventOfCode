3da22a40bd096587219abe7fe5a4193d44ea5680
Aaron Clawson <3766680+MadVikingGod@users.noreply.github.com>
2022-12-06 08:47:29 -0600

    make hasRepeat faster
    Signed-off-by: Aaron Clawson <3766680+MadVikingGod@users.noreply.github.com>

--- 2022/day6/main.go
+++ 2022/day6/main.go
@@ -31,12 +31,12 @@ func findStartOfMessage(s string) int {
 }
 
 func hasRepeat(s string) bool {
-	for i := 0; i < len(s)-1; i++ {
-		for j := i + 1; j < len(s); j++ {
-			if s[i] == s[j] {
-				return true
-			}
+	set := make(map[rune]bool)
+	for _, r := range s {
+		if set[r] {
+			return true
 		}
+		set[r] = true
 	}
 	return false
 }
